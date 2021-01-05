7b67c5c10eb536ddc6d8969b87cb44474a87b6a1
jvrcavalcanti <jonnyvictor01@gmail.com>
2021-01-05 01:01:48 -0300

    Whoops

--- src/helper.php
+++ src/helper.php
@@ -16,22 +16,3 @@ function request($param = null)
 {
     return is_null($param) ? new Request($_REQUEST) : request()->get($param);
 }
-
-function app(): Router
-{
-    if (!isset($GLOBALS['router']) && !isset($GLOBALS['app'])) {
-        throw new \Exception("Not exists router/app in global scope");
-    }
-
-    return $GLOBALS['router'] ?? $GLOBALS['app'];
-}
-
-function container($id = null)
-{
-    return is_null($id) ? app()->getContainer() : container()->get($id);
-}
-
-function resolve(string $class)
-{
-    return container()->make($class);
-}
